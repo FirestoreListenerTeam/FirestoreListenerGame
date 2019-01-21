b7bb064a59c18fcdc5e1decb327289cdb8fec673
Guillermo García Subirana <guillermogarsub@gmail.com>
2019-01-21 15:30:58 +0100

    main menu

--- FirestoreListenerGame/Assets/Scripts/MainMenuHandler.cs
+++ FirestoreListenerGame/Assets/Scripts/MainMenuHandler.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class MainMenuHandler : MonoBehaviour {
 
-    public GameObject play_btn;
+    public GameObject start_btn;
     public GameObject credits_btn;
     public GameObject how_to_play_btn;
     public GameObject exit_btn;
+
+    // XInput stuff
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +23,55 @@ public class MainMenuHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        // Detect if a button was pressed this frame
+
+        if (prevState.Buttons.A == ButtonState.Released && state.Buttons.A == ButtonState.Pressed)
+        {
+            // A Pressed
+            print("A PRESSED");
+        }
+
+        if (prevState.Buttons.B == ButtonState.Released && state.Buttons.B == ButtonState.Pressed)
+        {
+            // B Pressed
+
+        }
+
+        if (prevState.Buttons.X == ButtonState.Released && state.Buttons.X == ButtonState.Pressed)
+        {
+            // X Pressed
+
+        }
+
+        if (prevState.Buttons.Y == ButtonState.Released && state.Buttons.Y == ButtonState.Pressed)
+        {
+            // Y Pressed
+
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            // Like A Pressed
+            print("A PRESSED");
+        }
 	}
 }
