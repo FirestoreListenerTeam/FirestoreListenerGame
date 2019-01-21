930761d2cf98beaf68d14dbc5f2b47bb61427a18
Guillermo García Subirana <guillermogarsub@gmail.com>
2019-01-21 10:35:17 +0100

    Box dropping down done

--- FirestoreListenerGame/Assets/Scripts/GameController.cs
+++ FirestoreListenerGame/Assets/Scripts/GameController.cs
@@ -11,7 +11,10 @@ public class GameController : MonoBehaviour {
     public GameObject chair_4;
 
     //defines
-    public int box_y_spawn = 5;
+    public float box_y_spawn_position = 5f;
+    public float box_y_velocity = 0.2f;
+
+    private bool box_spawned = false;
 
     static 
 
@@ -25,22 +28,33 @@ public class GameController : MonoBehaviour {
         if (Input.GetKeyDown("1"))
         {
             print("Spawn box in chair 1");
-            box.transform.position = new Vector3(chair_1.transform.position.x, chair_1.transform.position.y + box_y_spawn, chair_1.transform.position.z);
+            box.transform.position = new Vector3(chair_1.transform.position.x, chair_1.transform.position.y + box_y_spawn_position, chair_1.transform.position.z);
+            box_spawned = true;
         }
         if (Input.GetKeyDown("2"))
         {
             print("Spawn box in chair 2");
-            box.transform.position = new Vector3(chair_2.transform.position.x, chair_2.transform.position.y + box_y_spawn, chair_2.transform.position.z);
+            box.transform.position = new Vector3(chair_2.transform.position.x, chair_2.transform.position.y + box_y_spawn_position, chair_2.transform.position.z);
+            box_spawned = true;
         }
         if (Input.GetKeyDown("3"))
         {
             print("Spawn box in chair 3");
-            box.transform.position = new Vector3(chair_3.transform.position.x, chair_3.transform.position.y + box_y_spawn, chair_3.transform.position.z);
+            box.transform.position = new Vector3(chair_3.transform.position.x, chair_3.transform.position.y + box_y_spawn_position, chair_3.transform.position.z);
+            box_spawned = true;
         }
         if (Input.GetKeyDown("4"))
         {
             print("Spawn box in chair 4");
-            box.transform.position = new Vector3(chair_4.transform.position.x, chair_4.transform.position.y + box_y_spawn, chair_4.transform.position.z);
+            box.transform.position = new Vector3(chair_4.transform.position.x, chair_4.transform.position.y + box_y_spawn_position, chair_4.transform.position.z);
+            box_spawned = true;
         }
+
+        if(box_spawned){ // Make the box go down till the chair
+            box.transform.position = new Vector3(box.transform.position.x, box.transform.position.y - box_y_velocity, box.transform.position.z);
+            if(box.transform.position.y < 2f)
+                box_spawned = false;
+        }
+
 	}
 }
