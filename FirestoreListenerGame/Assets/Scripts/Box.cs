04ecfc23d69a83ac22b0edf9a6f6ea07ff598883
DatBeQuiet <kayter72@gmail.com>
2019-01-21 12:00:26 +0100

    heart vibration with timestep normalized with currentloaded

--- FirestoreListenerGame/Assets/Scripts/Box.cs
+++ FirestoreListenerGame/Assets/Scripts/Box.cs
@@ -1,22 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using XInputDotNetPure;
 
-public class Box : MonoBehaviour {
+public class Box : MonoBehaviour
+{
 
-    float maxToLoad = 100.0f;
-    public float perCentLoaded = 0.0f;
-    public float normalizedLoaded;
+    public float maxToLoad = 100.0f;
+    public float heartBeatValue = 0.25f;
+    public float heartBeatTimeStep = 2.0f;
 
-    float timerHearth = 0.0f;
+    private float currentLoaded = 0.0f;
+
+    #region HeartVibVars
+    private float normalizedLoaded;
+    private float normalizedTimeStepLoaded;
+    float timerStepHeart = 0.0f;
+    float timerHeartVibration = 0.0f;
+    #endregion
 
     bool playerIndexSet = false;
     PlayerIndex playerIndex;
     GamePadState state;
     GamePadState prevState;
 
-    void Update () {
+    void Update()
+    {
 
         if (!playerIndexSet || !prevState.IsConnected)
         {
@@ -38,17 +45,39 @@ public class Box : MonoBehaviour {
 
         if (prevState.Buttons.A == ButtonState.Released && state.Buttons.A == ButtonState.Pressed)
         {
-            perCentLoaded += 5.0f;
-            normalizedLoaded = perCentLoaded * 1.0f / maxToLoad;
+            currentLoaded += 5.0f;
+
+            // normalized values
+            normalizedLoaded = currentLoaded * 1.0f / maxToLoad;
+            normalizedTimeStepLoaded = currentLoaded * heartBeatTimeStep / maxToLoad;
+            normalizedTimeStepLoaded = heartBeatTimeStep - normalizedTimeStepLoaded;
         }
 
-        timerHearth += 1.0f * Time.deltaTime;
+        HeartVibration();
+    }
 
-        if (timerHearth > 1.0f)
+    void HeartVibration()
+    {
+        timerStepHeart += 1.0f * Time.deltaTime;
+
+        if (timerStepHeart > normalizedTimeStepLoaded)
         {
-            GamePad.SetVibration(0, normalizedLoaded, normalizedLoaded);
-            timerHearth = 0.0f;
-        }     
+            timerHeartVibration += 1.0f * Time.deltaTime;
 
+            if (timerHeartVibration < heartBeatValue)
+            {
+                GamePad.SetVibration(0, 0, normalizedLoaded);
+            }
+            else if (timerHeartVibration >= heartBeatValue && timerHeartVibration <= (heartBeatValue * 2))
+            {
+                GamePad.SetVibration(0, normalizedLoaded, 0);
+            }
+            else
+            {
+                GamePad.SetVibration(0, 0, 0);
+                timerStepHeart = 0.0f;
+                timerHeartVibration = 0.0f;
+            }
+        }
     }
 }
