1cf39d412979f81a16b42b1a046620292b642f32
Guillermo García Subirana <guillermogarsub@gmail.com>
2019-01-21 12:22:52 +0100

    idle + squishy

--- FirestoreListenerGame/Assets/Scripts/GameController.cs
+++ FirestoreListenerGame/Assets/Scripts/GameController.cs
@@ -23,7 +23,6 @@ public class GameController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         // Get any component if needed
-        box_animator.StopPlayback();
 	}
 	
 	// Update is called once per frame
@@ -31,24 +30,28 @@ public class GameController : MonoBehaviour {
         if (Input.GetKeyDown("1"))
         {
             print("Spawn box in chair 1");
+            box_animator.SetBool("squishy", false);
             box.transform.position = new Vector3(chair_1.transform.position.x, chair_1.transform.position.y + box_y_spawn_position, chair_1.transform.position.z);
             box_spawned = true;
         }
         if (Input.GetKeyDown("2"))
         {
             print("Spawn box in chair 2");
+            box_animator.SetBool("squishy", false);
             box.transform.position = new Vector3(chair_2.transform.position.x, chair_2.transform.position.y + box_y_spawn_position, chair_2.transform.position.z);
             box_spawned = true;
         }
         if (Input.GetKeyDown("3"))
         {
             print("Spawn box in chair 3");
+            box_animator.SetBool("squishy", false);
             box.transform.position = new Vector3(chair_3.transform.position.x, chair_3.transform.position.y + box_y_spawn_position, chair_3.transform.position.z);
             box_spawned = true;
         }
         if (Input.GetKeyDown("4"))
         {
             print("Spawn box in chair 4");
+            box_animator.SetBool("squishy", false);
             box.transform.position = new Vector3(chair_4.transform.position.x, chair_4.transform.position.y + box_y_spawn_position, chair_4.transform.position.z);
             box_spawned = true;
         }
@@ -58,8 +61,7 @@ public class GameController : MonoBehaviour {
             if (box.transform.position.y < 2f)
             {
                 box_spawned = false;
-                //box_animator.Play()
-                box_animator.Play("box_squishy");
+                box_animator.SetBool("squishy", true);
             }
         }
 
