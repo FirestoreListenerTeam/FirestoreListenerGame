c3d1617ee04080ba2f8d349ce27b4a4e3d613319
Guillermo García Subirana <guillermogarsub@gmail.com>
2019-01-21 23:39:32 +0100

    Added script to have persistence between scenes

---
+++ FirestoreListenerGame/Assets/Scripts/Knowledge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knowledge : MonoBehaviour {
+    
+    private static int winner; // 1 - blue 2 - red 3 - yellow 4 - green
+
+    public static int Winner
+    {
+        get
+        {
+            return winner;
+        }
+        set
+        {
+            winner = value;
+        }
+    }
+
+}
