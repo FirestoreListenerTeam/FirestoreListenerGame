19047983f052c0f3df83c3ce8ab45b2c4997abf8
DatBeQuiet <kayter72@gmail.com>
2019-01-21 13:13:36 +0100

    basic menu

---
+++ FirestoreListenerGame/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    private GameObject mainMenu;
+    private GameObject playerSubMenu;
+
+    private void Start()
+    {
+        mainMenu = transform.Find("MainMenu").gameObject;
+        playerSubMenu = transform.Find("PlayersMenu").gameObject;
+    }
+
+    public void OnPlayClicked()
+    {
+        mainMenu.SetActive(false);
+        playerSubMenu.SetActive(true);
+    }
+
+    public void OnCreditsClicked()
+    {
+
+    }
+
+    public void OnExitClicked()
+    {
+
+    }
+}
